380a5de856144bb5f445e44054b5df0a16b816ad
lisuuu96 <adrian.lis1996@gmail.com>
2022-12-31 18:10:10 +0100

    result

---
+++ main.js
@@ -0,0 +1,6 @@
+let text = 'Uwielbiam Javascript'
+let textt = ' Jestem swietnym programista'
+
+function length(text,textt) {if(text.length>textt.length)return result}
+length(text,textt)
+console.log(length)
