a32dd136ae25a427c0c26b5a703da5a6002b34da
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-05 08:28:23 +0100

    length

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let text = 'Uwielbiam Javascript'
-let textt = ' Jestem swietnym programista'
+let message = ' Jestem swietnym programista'
 
-function length(text,textt) {if(text.length>textt.length)return result}
-length(text,textt)
+function length(text1,text2) {
+    if(text1.length>text2.length){
+        return text1;
+    }
+    else(text1.length<text2.length){
+        return text2;
+    }
+
+}
+length(text,message)
 console.log(length)
