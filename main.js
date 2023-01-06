d573fcaca0f15d9a8d5fb59352d47756fb16c880
lisuuu96 <120398096+lisuuu96@users.noreply.github.com>
2023-01-06 16:39:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function length(text1,text2) {
     if(text1.length>text2.length){
         return text1;
     }
-    else(text1.length<text2.length){
+    else if(text1.length<text2.length){
         return text2;
     }
 
